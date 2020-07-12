9418b317f6b51d09f020963438d5aebf93049fb3
kpeved <uakpevedko@gmail.com>
2020-07-13 00:08:11 +0100

    Mocked currencies server

--- feature_exchange/src/main/java/com/test/feature_exchange/repository/ExchangeRepository.kt
+++ feature_exchange/src/main/java/com/test/feature_exchange/repository/ExchangeRepository.kt
@@ -1,8 +1,11 @@
 package com.test.feature_exchange.repository
 
 import com.test.feature_exchange_network.service.ExchangeService
+import com.test.feature_exchange_network.service.MockedExchangeService
 
 class ExchangeRepository(private val service: ExchangeService) {
 
-    fun retrieveData(currency: String?) = service.getExchanges(currency)
+    // Server is not working anymore
+//    fun retrieveData(currency: String?) = service.getExchanges(currency)
+    fun retrieveData(currency: String?) = MockedExchangeService().getExchanges(currency)
 }
--- feature_exchange_network/src/main/java/com/test/feature_exchange_network/service/ExchangeService.kt
+++ feature_exchange_network/src/main/java/com/test/feature_exchange_network/service/ExchangeService.kt
@@ -4,8 +4,16 @@ import com.test.feature_exchange_network.model.CurrenciesModel
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.math.BigDecimal
 
 interface ExchangeService {
     @GET("latest")
     fun getExchanges(@Query("base") currency: String?): Single<CurrenciesModel>
+}
+
+class MockedExchangeService() : ExchangeService{
+    override fun getExchanges(currency: String?): Single<CurrenciesModel> {
+        return Single.just(CurrenciesModel("USD", rates = mapOf(Pair("EUR", BigDecimal(100)))))
+    }
+
 }
